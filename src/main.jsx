7a4a8d09554564e928c1c2380357eb83acf72581
ThevinduD <thevindud.work@gmail.com>
2025-04-03 13:43:20 +0530

    uploading to github

---
+++ src/main.jsx
@@ -0,0 +1,56 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css'
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Index from './routes';
+import ErrorPage from './error-page';
+import Contacts, { loader as contactLoader, action as contactAction } from './routes/contacts';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index/>
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contacts/>,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ]
+      }
+    ]
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+export default function Index() {
+    return (
+      <>
+        <p id="zero-state">
+          This is an online contact book <br />
+          Made using <span className="routerspan">React-Router</span> <br />
+          Feel free to look around.
+        </p>
+        <br /><br /><br />
+        <p className="copyright">Thevindu Dehigaspitiya Creation</p>
+      </>
+      
+    );
+  }
+  
